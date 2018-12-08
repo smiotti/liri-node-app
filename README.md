c5d195ba6a9fb87c288281e6187317a48566fef4
Miotti <210061319@LOGON.DS.GE.COM>
2018-12-08 18:27:54 -0500

    Updates to JS, Readme, and random.tx to demo app

--- README.md
+++ README.md
@@ -1,13 +1,85 @@
 # liri-node-app
-Assignment #10 - LIRI BOT
+### Assignment #10 - LIRI BOT
+
+Welcome to *Liri*, your personal search agent for music and movie infomation.  Similar to it's cousin Siri, Liri will take your command line input of a Song Name, Band Name, Artist, or a Movie Name and return usefull information about the band, song, or movie... like what upcoming venues a particular band will be performing.   The data behind this app is made avaible via API calls to the following data repositories:  
+
+* Bands in Town - http://www.artists.bandsintown.com/bandsintown-api
+* Spotify - https://www.npmjs.com/package/node-spotify-api
+* OMDB - http://www.omdbapi.com
+
+*This application is inteted for developer learning purposes, and not being published commercially, per 3rd party API guidelines.  All API keys/credentials for these data sources must be obtained by the user in order to run this application.*
 
 
+### How to use Liri:
+
+To use Liri you will need Node installed on your computure.  Open a command line window, and navigate to the root directory of the program.  Liri responds to four different commands. A desription of each command and links to screenshots of the program in action are provided below!  Use the following syntax from your command line to run Liri (without the quotes) -->  *node liri "command" "searchInfo"* 
+
+   * `concert-this` - This will search the Bands in Town Artist Events API for an artist/band and render the following information about each event to the command line:
+
+     * Name of the venue
+     * Venue location
+
+      [Example: node liri concert-this Fleetwood Mac](screen_cap/liri_concert-this.gif)
+
+        *If no artist or band is provided, or there are no upcoming events, then Liri will alert the user.*
+
+        [Example: node liri concert-this U2](screen_cap/liri_concert-this_no_data.GIF)
 
 
+   * `spotify-this-song` - This will search the Spotify API for a song (artist optional) and render the following information about the song to the command line:
 
-Add a `README.md` to your repository describing the project. Here are some resources for creating your `README.md`. Here are some resources to help you along the way:
+     * Artist(s)
+     * The song's name
+     * A preview link of the song from Spotify
+     * The album that the song is from
 
-* [About READMEs](https://help.github.com/articles/about-readmes/)
+      [Example: node liri spotify-this-song Dont stand so close to me](screen_cap/liri_spotify-this-song.gif)
 
-* [Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
+        *If you are getting unexpected results, you can strengthen your search by adding the artist name. To do this use a "," between song and artist.*
+        
+        [Example: node liri spotify-this-song Alone, Heart](screen_cap/liri_spotify-this-song_with_artis.gif)
+        
+        *If no song is provided then Liri will default to "What's My Age Again" by blink-182.*
 
+        [Example: node liri spotify-this-song ](screen_cap/liri_spotify-this-song_no_data.GIF)
+
+
+   * `movie-this` - This will search the OMDB API for a movie and render the following information about the movie to the command line:
+
+       * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie.
+
+        [Example: node liri movie-this Top Gun](screen_cap/liri_movie-this.gif)
+
+        *If the user doesn't type a movie in, Liri will output data for the movie 'Mr. Nobody.*
+
+        [Example: node liri movie-this ](screen_cap/liri_movie-this_no_data.gif)
+
+
+   * `do-what-it-says` - This will take the text inside of random.txt and then use it to call one of LIRI's commands above.
+
+        [Spotify Example: node liri do-what-it-says](screen_cap/liri_do-what-it-says_ex1.gif)
+
+        [Concert Example: node liri do-what-it-says](screen_cap/liri_do-what-it-says_ex2.gif)
+
+        [Movie Example: node liri do-what-it-says](screen_cap/liri_do-what-it-says_ex3.gif)
+
+        *If the random.txt file contains an invalid command, Liri will alert the user.*
+
+        [Invalid Data Example: node liri do-what-it-says](screen_cap/liri_do-what-it-says_invalid.gif)
+
+
+------
+  
+To contribute to the project, or get help -->   steve.miotti@dont_bother_emailing!!.com
+
+
+:bowtie:
+
+ 
--- liri.js
+++ liri.js
@@ -1,60 +1,60 @@
 
-//** Step #7 read and set any environment variables with the dotenv package https://www.npmjs.com/package/dotenv
-//** with npm, npm install dotenv
+// Setting any environment variables with the dotenv package https://www.npmjs.com/package/dotenv
 require('dotenv').config();
-var Spotify = require('node-spotify-api');
 
+// Setting up the Node Spotify API
+const Spotify = require('node-spotify-api');
 
-
-//** Step #8  import the `keys.js` file and store it in a variable
+// Import the `keys.js` file and store it in a variable
 const keys = require("./keys.js");
 
-//** Step #8 cont - access  keys information
+// Granting new instance of Spotify access to the keys information
 const spotify = new Spotify(keys.spotify);
 
-
-//** Include the request npm package to retrieve data from APIs  ( ran'npm install request' to install request package )
+// Include the request npm package to retrieve data from APIs
 const request = require('request');
 
-//** Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+// Setting up the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 const fs = require('fs');
 
 
-//** Take two arguments
-//** The first will be the action (i.e. 'concert-this', 'spotify-this-song', etc.)
-//** The second will be for the specific information to seach on (ie. Artist/Band Name, Song Name, Movie Name, etc. )
+// Take two user entered arguments from the command line
+// The first will be the action (i.e. 'concert-this', 'spotify-this-song', etc.)
+// The second will be for the specific information to seach on (ie. Artist/Band Name, Song Name, Movie Name, etc. )
+// Second argument will handle spaces in user imput
 let action = process.argv[2];
-let searchInfo = process.argv[3];
+let searchInfo = process.argv.slice(3).join(' ');
 
 
 
-//  1. `node liri.js concert-this <artist/band name here>`
-//    * This will search the Bands in Town Artist Events API (`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`) for an artist and render the following information about each event to the terminal:
-//      * Name of the venue
-//      * Venue location
-
-
-
-//** concert function
+// Concert Function
+// This function queries Bands in Town Artist Events API with an 'artis' or 'band name' and logs the 'name' and 'location' of the concert venue
 function concertThis() {
     const artist = searchInfo;
     const URL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
 
-    request(URL, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-
+    request(URL, function (err, response, body) {
+        // If there is an error in the API query the error code will be logged
+        if (err) {
+            return console.log(`Error occurred: ${err}`);
+        } else {
             const dataList = JSON.parse(body);
-            // console.log('error:', error);
-            // console.log('statusCode:', response && response.statusCode);
             // console.log('body:', dataList);
             console.log(`\n-------------- Concert This ---------------`);
             console.log(`Band Name: ${searchInfo}`);
             console.log(`-------------------------------------------`);
-            for (i = 0; i < dataList.length; i++) {
-                const details = dataList[i];
-                console.log(`\nVenue Name: ${details.venue.name}`);
-                console.log(`Venue Location: ${details.venue.city}, ${details.venue.region} ${details.venue.country}`);
-                console.log(`\n**********************************************`);
+
+            // If there is no venue information availalbe in Bands in Town, log a warning message to user, otherwise log venue name and location
+            if (!Array.isArray([]) || !dataList.length) {
+                console.log(`\n !! No Venue Information Availalbe For ${artist} !!`);
+
+            } else {
+                for (i = 0; i < dataList.length; i++) {
+                    const details = dataList[i];
+                    console.log(`\nVenue Name: ${details.venue.name}`);
+                    console.log(`Venue Location: ${details.venue.city}, ${details.venue.region} ${details.venue.country}`);
+                    console.log(`\n**********************************************`);
+                }
             }
         }
     });
@@ -62,34 +62,28 @@ function concertThis() {
 
 
 
-
-// 2. `node liri.js spotify-this-song '<song name here>'`
-// * This will show the following information about the song in your terminal/bash window
-// * Artist(s)
-// * The song's name
-// * A preview link of the song from Spotify
-// * The album that the song is from
-// * If no song is provided then your program will default to "What's My Age Again" by blink-182.
-// * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-// * The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
-// * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
-// * Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-// * Step Three: Once logged in, navigate to <https://developer.spotify.com/my-applications/#!/applications/create> to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-// * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
-
-
-
 // spotify function
+// This function queries the Spotify API with a 'song name' (or optioanl 'artist') then logs the Song Name, Artist Name, Preview Url and Album Name for that Song/Artis
 function spotifyThisSong() {
-    spotify.search({ type: 'track,artist', query: searchInfo, market: 'US' }, function (err, data) {
-        if (err ) {
-            return console.log('Error occurred: ' + err);
+    let songName;
+    // if user does not type a song name at command line, use the song "What's My Age Again" by blink-182, and returning Spotify data
+    if (!searchInfo) {
+        songName = "What's My Age Again"
+    } else {
+        songName = searchInfo;
+    }
+    // spotify package credentials provided via envionment file.  Using Node-spotify package query to search for user provided
+    // song name (artis optional) then log returned spotify data.  
+    // If there is a query errer log error code.
+    spotify.search({ type: 'track,artist', query: songName, market: 'US' }, function (err, data) {
+        if (err) {
+            return console.log(`Error occurred: ${err}`);
         } else {
             // console.log(data);
             // console.log(JSON.stringify(data.tracks.items[0], null, 2)); 
             console.log('\n************************ Spotify-This-Song *****************************\n')
-            console.log(`Artist Name:  ${JSON.stringify(data.tracks.items[0].album.artists[0].name)}`);
             console.log(`  Song Name:  ${JSON.stringify(data.tracks.items[0].name)}`);
+            console.log(`Artist Name:  ${JSON.stringify(data.tracks.items[0].album.artists[0].name)}`);
             console.log(`Preview Url:  ${JSON.stringify(data.tracks.items[0].href)}`);
             console.log(`      Album:  ${JSON.stringify(data.tracks.items[0].album.name)}`);
             console.log('\n************************************************************************\n')
@@ -98,99 +92,72 @@ function spotifyThisSong() {
 };
 
 
-// 3. `node liri.js movie-this '<movie name here>'`
-// * This will output the following information to your terminal/bash window:
-// ```
-//   * Title of the movie.
-//   * Year the movie came out.
-//   * IMDB Rating of the movie.
-//   * Rotten Tomatoes Rating of the movie.
-//   * Country where the movie was produced.
-//   * Language of the movie.
-//   * Plot of the movie.
-//   * Actors in the movie.
-// ```
-// * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-// * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-// * It's on Netflix!
-// * You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
-
 
 //movie function
+// This function queries the OMDB API with a user provided 'movie name' then logs the required related movie information
 function movieThis() {
-	let movieName;
-	if (!searchInfo) {
-		movieName = 'Mr. Nobody'
+    let movieName;
+    // if user does not type a movie name at command line, using the movie "Mr. Nobody", and returning imdb data
+    if (!searchInfo) {
+        movieName = 'Mr. Nobody'
     } else {
         movieName = searchInfo;
     }
 
     const movieSource = `http://www.omdbapi.com/?t=${movieName}&apikey=trilogy`;
 
-	request(movieSource, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			var info = JSON.parse(body);
-			// console.log('error:', error);
-			// console.log('statusCode:', response && response.statusCode);
+    // If there is a query errer log error code.
+    request(movieSource, function (err, response, body) {
+        if (err) {
+            return console.log(`Error occurred: ${err}`);
+        } else {
+            var info = JSON.parse(body);
             // console.log('body:', info);
             console.log('\n************************ Movie This *****************************\n')
-			console.log("                  Title of the movie: " + JSON.parse(body).Title);
-			console.log("             Year the movie came out: " + JSON.parse(body).Year);
-			console.log("            IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
-			console.log(" Rotton Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
-			console.log("Country where the movie was produced: " + JSON.parse(body).Country);
-			console.log("               Language of the movie: " + JSON.parse(body).Language);
+            console.log("                  Title of the movie: " + JSON.parse(body).Title);
+            console.log("             Year the movie came out: " + JSON.parse(body).Year);
+            console.log("            IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
+            console.log(" Rotton Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+            console.log("               Language of the movie: " + JSON.parse(body).Language);
             console.log("                 Actors in the movie: " + JSON.parse(body).Actors);
             console.log("                   Plot of the movie: " + JSON.parse(body).Plot);
-		}
-	});
+            console.log('\n******************************************************************\n')
+        }
+    });
 };
 
 
 
-
-
-
-
-
-
-// 4. `node liri.js do-what-it-says`
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-//      * Edit the text in random.txt to test out the feature for movie-this and my-tweets
-
-
-
-
-//doWhatItSays function
+// doWhatItSays function
+// This function uses the `fs` Node package to read a random.txt file and then uses it to call one of LIRI's commands: 'concert-this', 'spotify-this-song', or 'movie-this'.  
 function doWhatItSays() {
-	fs.readFile("random.txt", "utf8", function(error, data) {
-		if (error) {
-			return console.log(error);
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        // If there is a readFile errer log error code.
+        if (err) {
+            return console.log(`Error occurred: ${err}`);
         }
-        console.log(data);
-		let dataArr = data.split(",");
-		action = dataArr[0];
+        console.log(`\n\nCommand (from random.txt): ${data}\n`);
+        let dataArr = data.split(",");
+        action = dataArr[0];
         searchInfo = dataArr[1];
-        
-        if(action == "spotify-this-song"){
-         
+        // based on evaluated command from text file, a corresponding function call occurs
+        if (action == "concert-this") {
+            concertThis();
+        } else if (action == "spotify-this-song") {
             spotifyThisSong();
-        
+        } else if (action == "movie-this") {
+            movieThis();
         } else {
-            console.log("something else");
+            // if invalid liri command, log user message
+            console.log(`\n !! Invalid Liri Command. Check random.txt file. !!\n`);
         }
-        
-	})
+    })
 };
 
 
 
-
-
-// Step #9 Something like this from Day 2 - 04-stu ....   
-
-//**  Creating a switch-case statement to direct which function gets run.
+// Creating a switch-case statement to direct which function gets run, based on user input.
 switch (action) {
     case 'concert-this':
         concertThis();
@@ -207,17 +174,7 @@ switch (action) {
 }
 
 
-
-
-// To Do's:
-
-// Need to install 3rd party pacakges:
-// (done) npm install request   https://www.npmjs.com/package/request
-// (done) npm install dotenv   This file will be used by the `dotenv` package to set what are known as environment variables to the global `process.env` object in node. https://www.npmjs.com/package/dotenv
-// (done) npm install --save node-spotify-api  [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
-
-
-
-
-
-
+// toDos:  
+// Hide the keys from movie and concert APIs
+// Readme markup
+// Challenge
